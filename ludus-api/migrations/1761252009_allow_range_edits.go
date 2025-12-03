0b046c237076620c8d8652edcfd0465777cd3c26
Erik [Bad Sector Labs] <555113-badsectorlabs-erik@users.noreply.gitlab.com>
2025-12-03 17:00:11 -0500

    feat(api): ✨ allow users with access to ranges to list them/view them; allow managers or directly assigned users to update range name, description, or purpose directly via the pocketbase API
    Signed-off-by: Erik [Bad Sector Labs] <555113-badsectorlabs-erik@users.noreply.gitlab.com>

---
+++ ludus-api/migrations/1761252009_allow_range_edits.go
@@ -0,0 +1,75 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/types"
+
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		rangesCollection, err := app.FindCollectionByNameOrId("ranges")
+		if err != nil {
+			return err
+		}
+
+		// Allow access if the user is authenticated and is either directly assigned the range,
+		// or is a member/manager of a group that is assigned the range.
+		hasAccessRule := types.Pointer(`
+		@request.auth.id != "" && (
+			@request.auth.ranges ?= id || 
+			(
+				@collection.groups.ranges ?= id && 
+				(
+					@collection.groups.members ?= @request.auth.id || 
+					@collection.groups.managers ?= @request.auth.id
+				)
+			)
+		)`)
+
+		// Allow access if the user is authenticated and directly assigned the range
+		// or a manager of a group that is assigned the range
+		// We also check explicitly that the only fields that are allowed to be changed are: name, description, and purpose
+		// This is to prevent users from changing other fields that are not allowed to be changed directly via the API
+		isManagerOrDirectlyAssignedRule := types.Pointer(`
+		@request.auth.id != "" && (
+			@request.auth.ranges ?= id || 
+			(
+				@collection.groups.ranges ?= id && 
+				@collection.groups.managers ?= @request.auth.id
+			)
+		) &&
+		@request.body.rangeNumber:changed = false &&
+		@request.body.rangeID:changed = false &&
+		@request.body.lastDeployment:changed = false &&
+		@request.body.numberOfVMs:changed = false &&
+		@request.body.testingEnabled:changed = false &&
+		@request.body.allowedDomains:changed = false &&
+		@request.body.allowedIPs:changed = false &&
+		@request.body.rangeState:changed = false &&
+		@request.body.created:changed = false &&
+		@request.body.updated:changed = false
+		`)
+		rangesCollection.ListRule = hasAccessRule
+		rangesCollection.ViewRule = hasAccessRule
+		rangesCollection.UpdateRule = isManagerOrDirectlyAssignedRule
+		rangesCollection.DeleteRule = nil // Use the ludus handler for these operations
+		rangesCollection.CreateRule = nil
+
+		return app.Save(rangesCollection)
+	}, func(app core.App) error { // optional revert operation
+		rangesCollection, err := app.FindCollectionByNameOrId("ranges")
+		if err != nil {
+			return err
+		}
+
+		rangesCollection.ListRule = nil
+		rangesCollection.ViewRule = nil
+		rangesCollection.UpdateRule = nil
+		rangesCollection.DeleteRule = nil
+		rangesCollection.CreateRule = nil
+
+		return app.Save(rangesCollection)
+	})
+}
